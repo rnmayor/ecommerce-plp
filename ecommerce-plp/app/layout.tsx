b8fd26dbd0125fc867c152f441d1e24f51284d5e
Ronel Mayor <ronelmayor0428@gmail.com>
2026-02-17 16:37:43 +0800

    init: Setup of shadcn ui

--- ecommerce-plp/app/layout.tsx
+++ ecommerce-plp/app/layout.tsx
@@ -25,8 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+      suppressHydrationWarning
+    >
+      <body>{children}</body>
     </html>
   );
 }
--- ecommerce-plp/app/page.tsx
+++ ecommerce-plp/app/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from '@ecommerce/ui';
+
 export default function Home() {
-  return <div className="bg-[#540e02] text-white">Home Page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Button>Test</Button>
+      <Button variant="outline">Test</Button>
+      <Button variant="secondary">Test</Button>
+      <Button variant="ghost">Test</Button>
+      <Button variant="destructive">Test</Button>
+      <Button variant="link">Test</Button>
+      <Button size="xs">Test</Button>
+      <Button size="sm">Test</Button>
+      <Button size="lg">Test</Button>
+      <Button size="icon">Test</Button>
+      <Button size="icon-xs">Test</Button>
+      <Button size="icon-sm">Test</Button>
+      <Button size="icon-lg">Test</Button>
+    </div>
+  );
 }
